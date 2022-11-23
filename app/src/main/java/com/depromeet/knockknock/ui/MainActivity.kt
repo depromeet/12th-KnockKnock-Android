89d260c12909df3a177e17e51bba415ee0c9a78a
duru.hi <hwanggyuil@gmail.com>
2022-11-23 16:38:51 +0900

    [KnockKnock] 메인 > 메인화면 BottomNavigation 추가

---
+++ app/src/main/java/com/depromeet/knockknock/ui/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.depromeet.knockknock.ui
+
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.depromeet.knockknock.NavigationGraphDirections
+import com.depromeet.knockknock.R
+import com.depromeet.knockknock.base.BaseActivity
+import com.depromeet.knockknock.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main // get() : 커스텀 접근자, 코틀린 문법
+
+    override val viewModel: MainViewModel by viewModels()
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    var waitTime = 0L
+
+    override fun initStartView() {
+        initNavController()
+    }
+
+    override fun initDataBinding() {
+    }
+
+    override fun initAfterBinding() {
+    }
+
+    private fun initNavController() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> showBottomNav()
+                R.id.alarmRoomFragment -> showBottomNav()
+                R.id.myPageFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+        binding.bottomNavi.setupWithNavController(navController)
+
+        // 중복터치 막기!!
+        binding.bottomNavi.setOnItemReselectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.homeFragment -> {}
+                R.id.alarmRoomFragment -> {}
+                R.id.myPageFragment -> {}
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavi.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavi.visibility = View.GONE
+    }
+
+    override fun onBackPressed() {
+        try {
+            if (onBackPressedDispatcher.hasEnabledCallbacks()) {
+                super.onBackPressed()
+            } else {
+                when (navController.currentDestination?.id) {
+                    R.id.homeFragment -> {
+                        if(System.currentTimeMillis()-waitTime >= 1500) {
+                            waitTime = System.currentTimeMillis()
+                            Toast.makeText(this, getString(R.string.onBackPressed_Message), Toast.LENGTH_SHORT).show()
+                        } else {
+                            finishAffinity() // 액티비티 종료
+                        }
+                    }
+                    null -> super.onBackPressed()
+                    else -> navController.navigate(NavigationGraphDirections.actionMainFragment())
+                }
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/depromeet/knockknock/ui/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.depromeet.knockknock.ui
+
+import com.depromeet.knockknock.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : BaseViewModel() {
+
+    private val TAG = "MainViewModel"
+}
