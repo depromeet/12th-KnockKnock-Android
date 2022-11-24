06905aad733448323dfc519a05fadb974858a281
duru.hi <hwanggyuil@gmail.com>
2022-11-24 10:21:57 +0900

    [KnockKnock] 알림 설정 > 알림 설정 관련 String 추가

--- app/src/main/java/com/depromeet/knockknock/ui/alarmsetting/AlarmSettingFragment.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmsetting/AlarmSettingFragment.kt
@@ -1,6 +1,8 @@
 package com.depromeet.knockknock.ui.alarmsetting
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.depromeet.knockknock.R
 import com.depromeet.knockknock.base.BaseFragment
 import com.depromeet.knockknock.databinding.FragmentAlarmSettingBinding
@@ -17,17 +19,28 @@ class AlarmSettingFragment : BaseFragment<FragmentAlarmSettingBinding, AlarmSett
 
     override val viewModel : AlarmSettingViewModel by viewModels()
 
+    private val navController: NavController by lazy { findNavController() }
+
     override fun initStartView() {
         binding.apply {
             this.vm = viewModel
             this.lifecycleOwner = viewLifecycleOwner
         }
         exception = viewModel.errorEvent
+        initToolbar()
     }
 
     override fun initDataBinding() {
     }
 
     override fun initAfterBinding() {
+    }
+
+    private fun initToolbar() {
+        with(binding.toolbar) {
+            // 뒤로가기 버튼
+//            this.setNavigationIcon(R.drawable.ic_back)
+            this.setNavigationOnClickListener { navController.popBackStack() }
+        }
     }
 }
