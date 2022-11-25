e09af899888d87e0379c365b857fdd79520412d4
duru.hi <hwanggyuil@gmail.com>
2022-11-25 18:05:26 +0900

    [KnockKnock] 팝업 모달 > 팝업 모달 베이스 추가 (상황에 따라서 YellowAlert, RedAlert 사용하도록)

---
+++ app/src/main/java/com/depromeet/knockknock/base/BaseDialog.kt
@@ -0,0 +1,64 @@
+package com.depromeet.knockknock.base
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Point
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+
+abstract class BaseDialog<T : ViewDataBinding>(layoutId: Int) : DialogFragment(layoutId) {
+
+    private var _binding: T? = null
+    val binding get()= requireNotNull(_binding)
+
+    abstract val layoutResourceId: Int
+
+    abstract fun initStartView()
+
+    abstract fun initDataBinding()
+
+    abstract fun initAfterBinding()
+
+    private var isSetBackButtonValid = false
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutResourceId, container, false)
+        binding.lifecycleOwner = this
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initStartView()
+        initDataBinding()
+        initAfterBinding()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val windowManager = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val deviceWidth = size.x
+        params?.width = (deviceWidth * 0.9).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
