16ff971a9eca845842442ef795888d28db69e82c
JunJangE <wnswkd486@gmail.com>
2022-11-28 16:38:04 +0900

    feat: google-services 추가

---
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmcreate/AlarmCreateViewModel.kt
@@ -0,0 +1,13 @@
+package com.depromeet.knockknock.ui.alarmcreate
+
+import com.depromeet.knockknock.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AlarmCreateViewModel @Inject constructor(
+) : BaseViewModel() {
+
+    private val TAG = "AlarmRoomViewModel"
+
+}
