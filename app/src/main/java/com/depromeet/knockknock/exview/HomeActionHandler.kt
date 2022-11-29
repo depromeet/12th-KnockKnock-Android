2ffbedf8726fb5a29ee866aad3eb5e37e56e0289
JunJangE <wnswkd486@gmail.com>
2022-11-30 02:02:25 +0900

    comment: 빌드를 위해 주석처리

--- app/src/main/java/com/depromeet/knockknock/exview/HomeActionHandler.kt
+++ app/src/main/java/com/depromeet/knockknock/exview/HomeActionHandler.kt
@@ -1,10 +1,10 @@
-package com.dida.android.presentation.views.nav.home
-
-interface HomeActionHandler {
-    fun onHotItemClicked(cardId: Long)
-    fun onHotSellerItemClicked(userId: Int)
-    fun onSoldOutItemClicked(cardId: Long)
-    fun onCollectionItemClicked(userId: Int)
-    fun onSoldOutDayClicked(day: Int)
-    fun onUserFollowClicked(userId: Int)
-}
+//package com.dida.android.presentation.views.nav.home
+//
+//interface HomeActionHandler {
+//    fun onHotItemClicked(cardId: Long)
+//    fun onHotSellerItemClicked(userId: Int)
+//    fun onSoldOutItemClicked(cardId: Long)
+//    fun onCollectionItemClicked(userId: Int)
+//    fun onSoldOutDayClicked(day: Int)
+//    fun onUserFollowClicked(userId: Int)
+//}
--- data/src/main/java/com/depromeet/data/datasource/PhotoPagingSource.kt
+++ data/src/main/java/com/depromeet/data/datasource/PhotoPagingSource.kt
@@ -1,47 +1,47 @@
-package com.example.data.datasource.remote
-
-import androidx.paging.PagingSource
-import androidx.paging.PagingState
-import com.example.data.api.PhotoApi
-import com.example.data.datasource.remote.PhotoDataSourceImpl.Companion.NETWORK_PAGE_SIZE
-import com.example.data.mapper.toDomain
-import com.example.domain.model.Photo
-import retrofit2.HttpException
-import java.io.IOException
-import javax.inject.Inject
-
-private const val PHOTO_STARTING_PAGE_INDEX = 1
-
-class PhotoPagingSource @Inject constructor(
-    private val photoApi: PhotoApi
-) : PagingSource<Int, Photo>() {
-
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Photo> {
-        val position = params.key ?: PHOTO_STARTING_PAGE_INDEX
-        return try {
-            val response = photoApi.getPhotoList(position, params.loadSize).toDomain()
-
-            val nextKey = if (response.isEmpty()) {
-                null
-            } else {
-                position + (params.loadSize / NETWORK_PAGE_SIZE)
-            }
-            LoadResult.Page(
-                data = response,
-                prevKey = if (position == PHOTO_STARTING_PAGE_INDEX) null else position - 1,
-                nextKey = nextKey
-            )
-        } catch (exception: IOException) {
-            return LoadResult.Error(exception)
-        } catch (exception: HttpException) {
-            return LoadResult.Error(exception)
-        }
-    }
-    override fun getRefreshKey(state: PagingState<Int, Photo>): Int? {
-        return state.anchorPosition?.let { anchorPosition ->
-            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
-                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
-        }
-    }
-
-}
+//package com.example.data.datasource.remote
+//
+//import androidx.paging.PagingSource
+//import androidx.paging.PagingState
+//import com.example.data.api.PhotoApi
+//import com.example.data.datasource.remote.PhotoDataSourceImpl.Companion.NETWORK_PAGE_SIZE
+//import com.example.data.mapper.toDomain
+//import com.example.domain.model.Photo
+//import retrofit2.HttpException
+//import java.io.IOException
+//import javax.inject.Inject
+//
+//private const val PHOTO_STARTING_PAGE_INDEX = 1
+//
+//class PhotoPagingSource @Inject constructor(
+//    private val photoApi: PhotoApi
+//) : PagingSource<Int, Photo>() {
+//
+//    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Photo> {
+//        val position = params.key ?: PHOTO_STARTING_PAGE_INDEX
+//        return try {
+//            val response = photoApi.getPhotoList(position, params.loadSize).toDomain()
+//
+//            val nextKey = if (response.isEmpty()) {
+//                null
+//            } else {
+//                position + (params.loadSize / NETWORK_PAGE_SIZE)
+//            }
+//            LoadResult.Page(
+//                data = response,
+//                prevKey = if (position == PHOTO_STARTING_PAGE_INDEX) null else position - 1,
+//                nextKey = nextKey
+//            )
+//        } catch (exception: IOException) {
+//            return LoadResult.Error(exception)
+//        } catch (exception: HttpException) {
+//            return LoadResult.Error(exception)
+//        }
+//    }
+//    override fun getRefreshKey(state: PagingState<Int, Photo>): Int? {
+//        return state.anchorPosition?.let { anchorPosition ->
+//            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+//                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+//        }
+//    }
+//
+//}
