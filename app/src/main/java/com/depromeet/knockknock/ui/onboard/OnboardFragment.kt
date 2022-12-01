b5fdf700f7eec08537ebcf9581fa9ee4046f11d5
leeyjwinter <james_22@naver.com>
2022-12-01 17:51:48 +0900

    온보딩 > 온보딩 string res 저장

--- app/src/main/java/com/depromeet/knockknock/ui/onboard/OnboardFragment.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/onboard/OnboardFragment.kt
@@ -15,23 +15,19 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class OnboardFragment : Fragment() {
     private lateinit var binding : FragmentOnboardBinding
-    private val onboardAdapter = OnboardAdapter(
-        listOf(
-            OnboardData("푸시알림으로\n많은 사람에게 나의 말을\n전해보세요", R.drawable.onboard_img_1),
-            OnboardData("친구를 초대하고\n같은 관심사를 가진\n그룹을 만들어요",R.drawable.onboard_img_2),
-            OnboardData("이모지를 남기고\n푸시알림에\n생각을 표현해보세요",R.drawable.onboard_img_3)
-        )
-    )
-
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
 
     ): View? {
-        // Inflate the layout for this fragment
+        val onboardAdapter = OnboardAdapter(
+            listOf(
+                OnboardData(getString(R.string.onboard_description_one), R.drawable.onboard_img_1),
+                OnboardData(getString(R.string.onboard_description_two),R.drawable.onboard_img_2),
+                OnboardData(getString(R.string.onboard_description_three),R.drawable.onboard_img_3)
+            )
+        )
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_onboard,container,false)
         binding.introViewpager.adapter = onboardAdapter
         binding.springDotsIndicator.attachTo(binding.introViewpager)
