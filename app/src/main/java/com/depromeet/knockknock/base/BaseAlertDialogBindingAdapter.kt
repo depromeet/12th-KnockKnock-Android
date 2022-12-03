dbf13d3f966dec06c9358cfdb6033a2f900303ef
duru.hi <hwanggyuil@gmail.com>
2022-12-03 15:48:30 +0900

    [KnockKnock] 베이스 > DefaultAlertDialog 수정

--- app/src/main/java/com/depromeet/knockknock/base/BaseAlertDialogBindingAdapter.kt
+++ app/src/main/java/com/depromeet/knockknock/base/BaseAlertDialogBindingAdapter.kt
@@ -3,8 +3,6 @@ package com.depromeet.knockknock.base
 import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.depromeet.knockknock.R
-import com.depromeet.knockknock.databinding.DialogDefaultAlertBinding
 
 @BindingAdapter("textVisibleCheck")
 fun TextView.setTextVisibleCheck(contents: String?) {
