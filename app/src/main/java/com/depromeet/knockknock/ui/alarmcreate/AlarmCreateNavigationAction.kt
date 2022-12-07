1d4f225a66dc6cbea4bd471826dd109f55ca19c8
JunJangE <wnswkd486@gmail.com>
2022-12-07 20:13:47 +0900

    [KnockKnock] 알림 푸시 보내기 -> AlarmCreateNavigationAction 코드 수정 

--- app/src/main/java/com/depromeet/knockknock/ui/alarmcreate/AlarmCreateNavigationAction.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmcreate/AlarmCreateNavigationAction.kt
@@ -1,7 +1,6 @@
 package com.depromeet.knockknock.ui.alarmcreate
 
 sealed class AlarmCreateNavigationAction {
-    object NavigateToGallery : AlarmCreateNavigationAction()
     object NavigateToAlarmSend : AlarmCreateNavigationAction()
-
+    object NavigateToAddImage: AlarmCreateNavigationAction()
 }
