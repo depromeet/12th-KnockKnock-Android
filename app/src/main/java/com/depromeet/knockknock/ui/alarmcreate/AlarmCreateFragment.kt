a95822623d9817d9bb451fd01ac16b47d7768d02
JunJangE <wnswkd486@gmail.com>
2022-12-07 14:23:11 +0900

    [KnockKnock] 알림 푸시 보내기 ->  AlarmCreateFragment 코드 수정

--- app/src/main/java/com/depromeet/knockknock/ui/alarmcreate/AlarmCreateFragment.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmcreate/AlarmCreateFragment.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -38,60 +37,67 @@ class AlarmCreateFragment :
         initToolbar()
         setupEvent()
         setOnTouchListenerEditText()
-        countEditTextMessage()
     }
 
     private fun setupEvent() {
         lifecycleScope.launchWhenStarted {
             viewModel.navigationEvent.collectLatest {
                 when (it) {
-                    is AlarmCreateNavigationAction.NavigateToGallery -> readImage.launch("image/*")
+                    is AlarmCreateNavigationAction.NavigateToGallery -> photoAdd()
                     is AlarmCreateNavigationAction.NavigateToAlarmSend -> alarmSend()
                 }
             }
         }
     }
 
-    private fun alarmSend() {
-        val bottomSheet = BottomAlarmSend(
-            callback = {
-                when (it) {
-                    0 -> {
-                        // 예약 보내가 버튼 클릭
-                        alarmReservationSend()
-                    }
-                    1 -> {
-                        /**
-                         * 푸쉬알림을 발송했습니다!
-                         * fcm API!!
-                         * */
-
-                    }
+    private fun photoAdd() {
+        val bottomSheet = BottomImageAdd(callback = {
+            when (it) {
+                0 -> {
+                    // 갤러리로 이동
+                    readImage.launch("image/*")
+                }
+                1 -> {
+                    // 카메라를 통해 이미지 추가할 수 있게 끔 구현해야 함.
                 }
             }
-        )
+        })
+        bottomSheet.show(requireActivity().supportFragmentManager, TAG)
+    }
+
+    private fun alarmSend() {
+        val bottomSheet = BottomAlarmSend(callback = {
+            when (it) {
+                0 -> {
+                    // 예약 보내가 버튼 클릭
+                    alarmReservationSend()
+                }
+                1 -> {
+                    /**
+                     * 푸쉬알림을 발송했습니다!
+                     * fcm API!!
+                     * */
+
+                }
+            }
+        })
         bottomSheet.show(requireActivity().supportFragmentManager, TAG)
     }
 
     private fun alarmReservationSend() {
-        val bottomSheet = BottomAlarmReservationPicker(
-            callback = {
-                /**
-                 * 예약 푸쉬알림을 발송했습니다!
-                 * fcm API!!
-                 * */
-                Log.d("ttt", it.toString())
-            }
-        )
+        val bottomSheet = BottomAlarmReservationPicker(callback = {
+            /**
+             * 예약 푸쉬알림을 발송했습니다!
+             * fcm API!!
+             * */
+            Log.d("ttt", it.toString())
+        })
         bottomSheet.show(requireActivity().supportFragmentManager, TAG)
     }
 
-
     private val readImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
         uri?.let {
-            Glide.with(this)
-                .load(it)
-                .into(binding.imgLoad)
+            Glide.with(this).load(it).into(binding.imgLoad)
             viewModel.onImageStateChecked()
         }
     }
@@ -113,22 +119,9 @@ class AlarmCreateFragment :
         })
     }
 
-    private fun countEditTextMessage() {
-        lifecycleScope.launchWhenStarted {
-            viewModel.editTextMessageCountEvent.collectLatest {
-                binding.editTextCount.text = "$it/200"
+    override fun initDataBinding() {}
 
-                if (it == 0) binding.editTextCount.text =
-                    textChangeColor(binding.editTextCount, "#ff0000", 0, 1)
-            }
-        }
-    }
-
-    override fun initDataBinding() {
-    }
-
-    override fun initAfterBinding() {
-    }
+    override fun initAfterBinding() {}
 
     private fun initToolbar() {
         with(binding.toolbar) {
