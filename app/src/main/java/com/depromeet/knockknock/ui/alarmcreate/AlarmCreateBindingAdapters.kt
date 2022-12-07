537ef4bef721b202a8149a058df62f35d29a4d90
JunJangE <wnswkd486@gmail.com>
2022-12-07 20:14:19 +0900

    [KnockKnock] 알림 푸시 보내기 -> AlarmCreateBindingAdapters 코드 수정 

--- app/src/main/java/com/depromeet/knockknock/ui/alarmcreate/AlarmCreateBindingAdapters.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmcreate/AlarmCreateBindingAdapters.kt
@@ -1,11 +1,14 @@
 package com.depromeet.knockknock.ui.alarmcreate
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.Color
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -36,12 +39,6 @@ fun TextView.bindDeleteTextVisible(textLength: Int) {
     }
 }
 
-@BindingAdapter("linearLayoutColorChange")
-fun LinearLayout.bindLinearLayoutColorChange(textLength: Int) {
-    if (textLength == 0)  this.setBackgroundResource(R.drawable.custom_backgroundgray03_radius10)
-    else this.setBackgroundResource(R.drawable.custom_backgroundgray03_radius10_line_gray08)
-}
-
 @BindingAdapter("editTextCountColorChange")
 fun TextView.bindEditTextCountColorChange(textLength: Int) {
     this.text = "$textLength/200"
@@ -64,4 +61,13 @@ fun TextView.textChangeColor(
     )
 
     return builder
+}
+
+@SuppressLint("UseCompatLoadingForDrawables")
+fun EditText.customOnFocusChangeListener(context: Context, linearLayout: LinearLayout) {
+    this.onFocusChangeListener = View.OnFocusChangeListener { view, gainFocus ->
+        //포커스가 주어졌을 때
+        if (gainFocus) linearLayout.background = context.getDrawable(R.drawable.custom_backgroundgray03_radius10_line_gray08)
+        else linearLayout.background = context.getDrawable(R.drawable.custom_backgroundgray03_radius10)
+    }
 }
