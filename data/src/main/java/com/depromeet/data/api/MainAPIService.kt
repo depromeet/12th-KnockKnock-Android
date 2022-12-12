3011542386c95c43a2b510e398d2f9c04cfb8633
duru.hi <hwanggyuil@gmail.com>
2022-12-12 15:25:15 +0900

    [KnockKnock] Remote > API 주석 추가 & 토큰 재발급 Http Method 수정

--- data/src/main/java/com/depromeet/data/api/MainAPIService.kt
+++ data/src/main/java/com/depromeet/data/api/MainAPIService.kt
@@ -7,15 +7,20 @@ import com.depromeet.data.model.response.GetGoogleLoginResponse
 import com.depromeet.data.model.response.GetKakaoLoginResponse
 import com.depromeet.data.model.response.PostRefreshTokenResponse
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 
 interface MainAPIService {
-    @GET("/credentials/refresh")
+
+    // Refresh Token 재발급
+    @POST("/credentials/refresh")
     suspend fun postRefreshToken(body: PostRefreshTokenRequest): PostRefreshTokenResponse
 
+    // Oauth Kakao 로그인
     @GET("/credentials/oauth/kakao")
     suspend fun getKakaoLogin(body: GetKakaoLoginRequest): GetKakaoLoginResponse
 
+    // Oauth Google 로그인
     @GET("/credentials/oauth/google")
     suspend fun getGoogleLogin(body: GetGoogleLoginRequest): GetGoogleLoginResponse
 }
