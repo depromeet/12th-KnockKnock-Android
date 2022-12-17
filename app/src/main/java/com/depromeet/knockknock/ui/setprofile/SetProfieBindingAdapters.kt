c1c7b0349561aeb3fd0295f21aff94c0b1081893
leeyjwinter <james_22@naver.com>
2022-12-17 16:16:29 +0900

    온보딩 > merge 적용안된 파일 수정

--- app/src/main/java/com/depromeet/knockknock/ui/setprofile/SetProfieBindingAdapters.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/setprofile/SetProfieBindingAdapters.kt
@@ -1,28 +1,31 @@
 package com.depromeet.knockknock.ui.setprofile
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Build
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 import com.depromeet.knockknock.R
 
-@BindingAdapter("setBtnVisibie")
-fun TextView.bindSetBtnVisibie(enable: Boolean) {
-    if(enable) this.visibility = View.VISIBLE
-    else this.visibility = View.GONE
-}
+fun textChangeColor(
+    text: TextView,
+    color: String,
+    start: Int,
+    end: Int
+): SpannableStringBuilder {
+    val builder = SpannableStringBuilder(text.text.toString())
 
-@SuppressLint("UseCompatLoadingForDrawables")
-@RequiresApi(Build.VERSION_CODES.M)
-@BindingAdapter("setBtnBackgroundTextColor")
-fun TextView.bindSetBtnBackground(possible: Boolean) {
-    if(possible) {
-        this.background = context.getDrawable(R.drawable.custom_yellow_radius16)
-        this.setTextColor(context.getColor(R.color.black))
-    } else {
-        this.background = context.getDrawable(R.drawable.custom_backgroundwhite_radius16)
-        this.setTextColor(context.getColor(R.color.gray08))
-    }
+    builder.setSpan(
+        ForegroundColorSpan(Color.parseColor(color)),
+        start,
+        end,
+        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+
+    return builder
 }
