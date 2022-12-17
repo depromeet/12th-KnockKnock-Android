eb4953baeba8743f7836a2f9544f9519e1e93976
duru.hi <hwanggyuil@gmail.com>
2022-12-17 15:15:54 +0900

    [KnockKnock] 카카오 로그인 > 로그인 웹으로만 가입 가능하도록 수정

--- app/src/main/java/com/depromeet/knockknock/ui/register/KakaoAuthViewModel.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/register/KakaoAuthViewModel.kt
@@ -112,7 +112,6 @@ class KakaoAuthViewModel(application: Application) : AndroidViewModel(applicatio
                     }
                 }
 
-
                 UserApiClient.instance.me { user, error ->
                     if (error != null) {
                         Log.e(TAG, "사용자 정보 요청 실패", error)
@@ -130,27 +129,30 @@ class KakaoAuthViewModel(application: Application) : AndroidViewModel(applicatio
             }
         }
 
-// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
-                if (error != null) {
-                    Log.e(TAG, "카카오톡으로 로그인 실패", error)
+        // 1. 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인 -> 카카오 웹으로 무조건 이동
+        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
 
-                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        return@loginWithKakaoTalk
-                    }
-
-                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
-                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
-                } else if (token != null) {
-                    Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
-                }
-            }
-        } else {
-            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
-        }
+        // 2. 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+//        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+//            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+//                if (error != null) {
+//                    Log.e(TAG, "카카오톡으로 로그인 실패", error)
+//
+//                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+//                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+//                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+//                        return@loginWithKakaoTalk
+//                    }
+//
+//                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+//                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+//                } else if (token != null) {
+//                    Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+//                }
+//            }
+//        } else {
+//            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+//        }
 
     }
 }
