21559961680fd7c52073208bf22e98ffc952f337
junjange <wnswkd486@naver.com>
2022-12-18 17:49:09 +0900

    [KnockKnock] 푸시 알림 보내기 > PreviewBindingAdapters 추가

---
+++ app/src/main/java/com/depromeet/knockknock/ui/preview/PreviewBindingAdapters.kt
@@ -0,0 +1,23 @@
+package com.depromeet.knockknock.ui.preview
+
+import android.text.TextUtils
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.depromeet.knockknock.R
+
+//@BindingAdapter("viewMoreVisible")
+//fun ImageView.bindViewMoreVisible(clickable: Boolean, textView: TextView) {
+//    if (clickable){
+//        this.setImageResource(R.drawable.ic_arrow_down)
+//        textView.ellipsize = null
+//        textView.maxLines = Int.MAX_VALUE
+//    }
+//    else {
+//        this.setImageResource(R.drawable.ic_arrow_down)
+//        textView.ellipsize = TextUtils.TruncateAt.END
+//        textView.maxLines = 2
+//
+//    }
+//}
