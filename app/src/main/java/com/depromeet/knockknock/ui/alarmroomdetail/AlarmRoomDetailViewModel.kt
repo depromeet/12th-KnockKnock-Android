49b68f17d6af6d2a93de2ba53b45c57b034a546b
junjange <wnswkd486@naver.com>
2022-12-21 12:26:25 +0900

    [KnockKnock] 알림방 페이지 > AlarmRoomDetailViewModel init

---
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmroomdetail/AlarmRoomDetailViewModel.kt
@@ -0,0 +1,14 @@
+package com.depromeet.knockknock.ui.alarmroomdetail
+
+import com.depromeet.knockknock.base.BaseViewModel
+//import com.dida.android.presentation.views.nav.home.HomeNavigationAction
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AlarmRoomDetailViewModel @Inject constructor(
+) : BaseViewModel() {
+
+    private val TAG = "AlarmRoomDetailViewModel"
+
+}
