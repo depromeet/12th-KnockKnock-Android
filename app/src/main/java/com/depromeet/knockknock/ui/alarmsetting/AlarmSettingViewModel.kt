332a3d9366cd2e4470df0da803157319c93ce77f
duru.hi <hwanggyuil@gmail.com>
2022-12-23 10:47:29 +0900

    [KnockKnock] 알림설정 > 알림설정 API 추가

--- app/src/main/java/com/depromeet/knockknock/ui/alarmsetting/AlarmSettingViewModel.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmsetting/AlarmSettingViewModel.kt
@@ -2,14 +2,18 @@ package com.depromeet.knockknock.ui.alarmsetting
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.depromeet.domain.onSuccess
+import com.depromeet.domain.repository.MainRepository
 import com.depromeet.knockknock.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AlarmSettingViewModel @Inject constructor(
+    private val mainRepository: MainRepository
 ) : BaseViewModel(), AlarmSettingsActionHandler {
 
     private val TAG = "AlarmSettingViewModel"
@@ -24,12 +28,24 @@ class AlarmSettingViewModel @Inject constructor(
     val notReceivedPushAtNightPermitted: StateFlow<Boolean> = _notReceivedPushAtNightPermitted
 
     override fun onNewPushToggled(checked: Boolean) {
+        baseViewModelScope.launch {
+            if(checked) mainRepository.postOptionNew()
+            else mainRepository.deleteOptionNew()
+        }
     }
 
     override fun onReactionPushToggled(checked: Boolean) {
+        baseViewModelScope.launch {
+            if(checked) mainRepository.postOptionReaction()
+            else mainRepository.deleteOptionReaction()
+        }
     }
 
     override fun onNotReceivedPushAtNight(checked: Boolean) {
+        baseViewModelScope.launch {
+            if(checked) mainRepository.postOptionNight()
+            else mainRepository.deleteOptionNight()
+        }
     }
 
 }
