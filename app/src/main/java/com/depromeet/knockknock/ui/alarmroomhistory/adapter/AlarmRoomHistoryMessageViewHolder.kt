7cd9ff56de75f37493f5f751269e00bf1acef52a
junjange <wnswkd486@naver.com>
2022-12-24 12:39:53 +0900

    [KnockKnock] 알림방 페이지 > 더보기, 간단히 보기 기능 추가

--- app/src/main/java/com/depromeet/knockknock/ui/alarmroomhistory/adapter/AlarmRoomHistoryMessageViewHolder.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmroomhistory/adapter/AlarmRoomHistoryMessageViewHolder.kt
@@ -1,6 +1,7 @@
 package com.depromeet.knockknock.ui.alarmroomhistory.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import com.depromeet.knockknock.R
 import com.depromeet.knockknock.databinding.ItemRecycleHistoryMessageBinding
 import com.depromeet.knockknock.ui.alarmroomhistory.model.HistoryMessage
 
@@ -12,6 +13,21 @@ class AlarmRoomHistoryMessageViewHolder(
         binding.apply {
             model = item
             executePendingBindings()
+            binding.expandBtn.setOnClickListener {
+                binding.model!!.isExpanded = !(binding.model!!.isExpanded)
+
+                if (binding.model!!.isExpanded) {
+                    binding.expandBtn.apply {
+                        this.text = context.getString(R.string.shorts_contents)
+                    }
+                    binding.contents.maxLines = 9999
+                } else {
+                    binding.expandBtn.apply {
+                        this.text = context.getString(R.string.more_contents)
+                    }
+                    binding.contents.maxLines = 2
+                }
+            }
         }
     }
 }
