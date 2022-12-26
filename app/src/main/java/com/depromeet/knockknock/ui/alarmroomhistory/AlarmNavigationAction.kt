bcd90c17802da323159bcd0e5070dfc8d55a176a
junjange <wnswkd486@naver.com>
2022-12-26 23:09:56 +0900

    [KnockKnock] 알림방 페이지 > NavigationAction 이름 변경

--- app/src/main/java/com/depromeet/knockknock/ui/alarmroomhistory/AlarmNavigationAction.kt
+++
@@ -1,9 +0,0 @@
-package com.depromeet.knockknock.ui.alarmroomhistory
-
-sealed class AlarmNavigationAction {
-    object NavigateToBookmarkEdit: AlarmNavigationAction()
-    object NavigateToBookmarkFilterReset: AlarmNavigationAction()
-    object NavigateToBookmarkFilterRoom: AlarmNavigationAction()
-    object NavigateToBookmarkFilterPeriod: AlarmNavigationAction()
-    class NavigateToReaction(val bookmarkIdx: Int) : AlarmNavigationAction()
-}
