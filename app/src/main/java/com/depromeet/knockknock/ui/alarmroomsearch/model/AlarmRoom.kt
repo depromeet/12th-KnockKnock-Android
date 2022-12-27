ea2d548fe8dd11d330f75f2fe8f02b6f64732b3c
leeyjwinter <james_22@naver.com>
2022-12-27 18:46:01 +0900

    알림방 model Alarm Room : background src 받는 parameter 생성

--- app/src/main/java/com/depromeet/knockknock/ui/alarmroomsearch/model/AlarmRoom.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmroomsearch/model/AlarmRoom.kt
@@ -1,12 +1,15 @@
 package com.depromeet.knockknock.ui.alarmroomsearch.model
 
+import com.depromeet.knockknock.ui.editroomdetails.model.Background
+
 data class AlarmRoom(
-    val roomType: String, //홀로외침방인지, 친구방인지
+    val roomType: String = "OPEN", //홀로외침방인지, 친구방인지
     val roomId: Int,
     val roomName: String,
+    val roomBackground: String? = null,
     val roomThumbnail: String,
     val roomDescription: String,
-    val roomIsUnpublic: Boolean,
+    val roomIsUnpublic: Boolean = true,
     val roomCategoryName : String,
     val roomMemberCount : Int
 )
