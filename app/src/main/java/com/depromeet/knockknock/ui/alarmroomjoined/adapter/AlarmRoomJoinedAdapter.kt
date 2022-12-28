3bfa6fb35a060f0a1398bd2f2358cdebd7ebcfc1
leeyjwinter <james_22@naver.com>
2022-12-28 17:58:03 +0900

    알림방 탐색(참여중) : UI 생성 및 뷰홀더 생성

---
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmroomjoined/adapter/AlarmRoomJoinedAdapter.kt
@@ -0,0 +1,57 @@
+package com.depromeet.knockknock.ui.alarmroomjoined.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.depromeet.knockknock.R
+import com.depromeet.knockknock.databinding.HolderAlarmRoomBinding
+import com.depromeet.knockknock.ui.alarmroomjoined.AlarmRoomJoinedActionHandler
+import com.depromeet.knockknock.ui.alarmroomsearch.model.AlarmRoom
+
+class AlarmRoomJoinedAdapter(
+    private val eventListener: AlarmRoomJoinedActionHandler
+) : ListAdapter<AlarmRoom, AlarmRoomJoinedAdapter.ViewHolder>(AlarmRoomListItemDiffCallback){
+
+    init { setHasStableIds(true) }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val viewDataBinding: HolderAlarmRoomBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.holder_alarm_room,
+            parent,
+            false
+        )
+        viewDataBinding.layoutMain.setOnClickListener {
+            eventListener.onRoomClicked(viewDataBinding.holder!!.roomId)
+            this.notifyDataSetChanged()
+        }
+        return ViewHolder(viewDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(private val binding: HolderAlarmRoomBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("UseCompatLoadingForDrawables")
+        fun bind(item: AlarmRoom) {
+            binding.holder = item
+            binding.executePendingBindings()
+        }
+    }
+
+    internal object AlarmRoomListItemDiffCallback : DiffUtil.ItemCallback<AlarmRoom>() {
+        override fun areItemsTheSame(oldItem: AlarmRoom, newItem: AlarmRoom) =
+            oldItem.roomId == newItem.roomId
+
+        override fun areContentsTheSame(oldItem: AlarmRoom, newItem: AlarmRoom) =
+            oldItem == newItem
+    }
+}
