dd7e865d2517557162a2ed673ee4fbbf8ce6e2e6
duru.hi <hwanggyuil@gmail.com>
2023-01-06 16:37:51 +0900

    [KnockKnock] 홈 > 사용 안하는 Model 제거

--- app/src/main/java/com/depromeet/knockknock/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/home/HomeViewModel.kt
@@ -4,7 +4,6 @@ import com.depromeet.domain.model.Notification
 import com.depromeet.domain.onSuccess
 import com.depromeet.domain.repository.MainRepository
 import com.depromeet.knockknock.base.BaseViewModel
-import com.depromeet.knockknock.ui.bookmark.model.Room
 import com.depromeet.knockknock.util.randomNum
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
@@ -20,9 +19,6 @@ class HomeViewModel @Inject constructor(
 
     private val _navigationHandler: MutableSharedFlow<HomeNavigationAction> = MutableSharedFlow<HomeNavigationAction>()
     val navigationHandler: SharedFlow<HomeNavigationAction> = _navigationHandler.asSharedFlow()
-
-    private val _roomList: MutableStateFlow<List<Room>> = MutableStateFlow<List<Room>>(emptyList())
-    val roomList: StateFlow<List<Room>> = _roomList.asStateFlow()
 
     val homeRandomNumber: Int = randomNum()
     val recommendText: String = "똑똑!\n오늘은 누구의 문을\n두드려 볼까!?"
