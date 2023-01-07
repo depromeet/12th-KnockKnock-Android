ad60873458416b0a4f84e8a6313eada25a2d9511
leeyjwinter <james_22@naver.com>
2023-01-07 22:59:50 +0900

    홀로외침방 만들기 : 카테고리 설정 UI 구현

---
+++ app/src/main/java/com/depromeet/knockknock/ui/aloneroommakecategory/AloneRoomMakeCategoryBindingAdapters.kt
@@ -0,0 +1,29 @@
+package com.depromeet.knockknock.ui.aloneroommakecategory
+
+import android.annotation.SuppressLint
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
+import com.depromeet.knockknock.R
+
+@SuppressLint("UseCompatLoadingForDrawables")
+@BindingAdapter("saveBtnEnable")
+fun TextView.bindSaveBtnEnable(enable: Boolean) {
+    if(enable) {
+        this.setTextColor(context.getColor(R.color.black))
+        this.background = context.getDrawable(R.drawable.custom_yellow_radius10)
+    } else {
+        this.setTextColor(context.getColor(R.color.gray08))
+        this.background = context.getDrawable(R.drawable.custom_backgroundwhite_radius10)
+    }
+}
+
+@SuppressLint("UseCompatLoadingForDrawables")
+@BindingAdapter("categoryClicked")
+fun ConstraintLayout.bindCategoryClicked(checked: Boolean) {
+    if(checked) {
+        this.background = context.getDrawable(R.drawable.custom_yellow_radius16)
+    } else {
+        this.background = context.getDrawable(R.drawable.custom_transparents_radius16_line_gray04)
+    }
+}
