f6ae882af68b7938a701db0ee047d7da5b92203d
최현정 <hjchoi.dev@gmail.com>
2023-01-09 22:39:32 +0900

    [KnockKnock] 만든 사람들 > Github 링크 추가 완료

--- app/src/main/java/com/depromeet/knockknock/ui/makers/MakersFragment.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/makers/MakersFragment.kt
@@ -1,5 +1,7 @@
 package com.depromeet.knockknock.ui.makers
 
+import android.content.Intent
+import android.net.Uri
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -28,6 +30,7 @@ class MakersFragment :
         }
         exception = viewModel.errorEvent
         initToolbar()
+        clickWhoIAm()
     }
 
     override fun initDataBinding() {
@@ -50,5 +53,54 @@ class MakersFragment :
     }
 
     override fun initAfterBinding() {
+    }
+
+    private fun clickWhoIAm() {
+        // 정은님 Github
+        binding.ivMakersServer01Link.setOnClickListener {
+            showGithub("mybloom")
+        }
+        // 서준님 Github
+        binding.ivMakersServer02Link.setOnClickListener {
+            showGithub("leeseojune53")
+        }
+        // 찬진님 Github
+        binding.ivMakersServer03Link.setOnClickListener {
+            showGithub("ImNM")
+        }
+        // 영준님 Github
+        binding.ivMakersAos01Link.setOnClickListener {
+            showGithub("leeyjwinter")
+        }
+        // 준장님 Github
+        binding.ivMakersAos02Link.setOnClickListener {
+            showGithub("junjange")
+        }
+        // 현정님 Github
+        binding.ivMakersAos03Link.setOnClickListener {
+            showGithub("hyunjung-choi")
+        }
+        // 규일님 Github
+        binding.ivMakersAos04Link.setOnClickListener {
+            showGithub("Gyuil-Hwnag")
+        }
+        // 나영님
+        binding.ivMakersDesign01Link.setOnClickListener {
+
+        }
+        // 수연님
+        binding.ivMakersDesign02Link.setOnClickListener {
+
+        }
+        // 승희님
+        binding.ivMakersDesign03Link.setOnClickListener {
+
+        }
+    }
+
+    private fun showGithub(githubId: String) {
+        val url = "https://github.com/"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("${url}${githubId}"))
+        startActivity(intent)
     }
 }
