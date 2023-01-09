4c22d837515bc10566066e527472aa1475091c7f
duru.hi <hwanggyuil@gmail.com>
2023-01-09 14:27:04 +0900

    [KnockKnock] 북마크 > 북마크 리액션 관련 Adapter 수정

--- domain/src/main/java/com/depromeet/domain/model/ReactionCountInfo.kt
+++ domain/src/main/java/com/depromeet/domain/model/ReactionCountInfo.kt
@@ -3,5 +3,6 @@ package com.depromeet.domain.model
 data class ReactionCountInfo(
     val notification_id: Int,
     val reaction_count: Int,
+    val reaction_url: String,
     val reaction_id: Int
 )
