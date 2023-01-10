f2094c2c3e5506882515b02b46342488e3353760
junjange <wnswkd486@naver.com>
2023-01-10 10:33:03 +0900

    [KnockKnock] 알림방 페이지 > AlarmRoomHistoryMessagePagingSource 파일명 변경

--- app/src/main/java/com/depromeet/knockknock/ui/alarmroomhistory/adapter/AlarmRoomHistoryMessagePagingSource.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmroomhistory/adapter/AlarmRoomHistoryMessagePagingSource.kt
@@ -1,7 +1,6 @@
 package com.depromeet.knockknock.ui.alarmroomhistory.adapter
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
