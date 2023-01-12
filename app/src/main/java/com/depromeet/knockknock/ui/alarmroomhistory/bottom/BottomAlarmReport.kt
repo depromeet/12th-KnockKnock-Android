d9425f981698b52b2ff6b233d1046b4402794a7c
junjange <wnswkd486@naver.com>
2023-01-12 15:32:09 +0900

    [KnockKnock] 알림방 페이지 > 알림 신고하기 UI 수정

--- app/src/main/java/com/depromeet/knockknock/ui/alarmroomhistory/bottom/BottomAlarmReport.kt
+++ app/src/main/java/com/depromeet/knockknock/ui/alarmroomhistory/bottom/BottomAlarmReport.kt
@@ -11,6 +11,7 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.depromeet.knockknock.R
 import com.depromeet.knockknock.ui.alarmcreate.messageTextOnFocusChangeListener
@@ -23,45 +24,46 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomAlarmReport(
     val callback: (period: String) -> Unit
-) : BottomSheetDialogFragment(){
-    private lateinit var dlg : BottomSheetDialog
+) : BottomSheetDialogFragment() {
+    private lateinit var dlg: BottomSheetDialog
 
     private var spemType: PeriodType = PeriodType.Init
 
     private val all by lazy { requireView().findViewById<ConstraintLayout>(R.id.zero_btn) }
     private val allCheck by lazy { requireView().findViewById<CheckBox>(R.id.zero_check) }
 
-    private val one by lazy {  requireView().findViewById<ConstraintLayout>(R.id.one_btn) }
-    private val oneCheck by lazy {  requireView().findViewById<CheckBox>(R.id.one_check) }
+    private val one by lazy { requireView().findViewById<ConstraintLayout>(R.id.one_btn) }
+    private val oneCheck by lazy { requireView().findViewById<CheckBox>(R.id.one_check) }
 
-    private val three by lazy {  requireView().findViewById<ConstraintLayout>(R.id.three_btn) }
-    private val threeCheck by lazy {  requireView().findViewById<CheckBox>(R.id.three_check) }
+    private val three by lazy { requireView().findViewById<ConstraintLayout>(R.id.three_btn) }
+    private val threeCheck by lazy { requireView().findViewById<CheckBox>(R.id.three_check) }
 
-    private val six by lazy {  requireView().findViewById<ConstraintLayout>(R.id.six_btn) }
-    private val sixCheck by lazy {  requireView().findViewById<CheckBox>(R.id.six_check) }
+    private val six by lazy { requireView().findViewById<ConstraintLayout>(R.id.six_btn) }
+    private val sixCheck by lazy { requireView().findViewById<CheckBox>(R.id.six_check) }
 
-    private val year by lazy {  requireView().findViewById<ConstraintLayout>(R.id.year_btn) }
-    private val yearCheck by lazy {  requireView().findViewById<CheckBox>(R.id.year_check) }
+    private val year by lazy { requireView().findViewById<ConstraintLayout>(R.id.year_btn) }
+    private val yearCheck by lazy { requireView().findViewById<CheckBox>(R.id.year_check) }
 
-    private val editTextReport by lazy {  requireView().findViewById<EditText>(R.id.edit_text_report) }
-    private val linearLayoutEditTextReport by lazy {  requireView().findViewById<LinearLayout>(R.id.linear_layout_edit_text_report) }
+    private val editTextReport by lazy { requireView().findViewById<EditText>(R.id.edit_text_report) }
+    private val linearLayoutEditTextReport by lazy { requireView().findViewById<LinearLayout>(R.id.linear_layout_edit_text_report) }
 
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         setStyle(STYLE_NORMAL, R.style.DialogStyle)
 
         // 이 코드를 실행하지 않으면 XML에서 round 처리를 했어도 적용되지 않는다.
-        dlg = ( super.onCreateDialog(savedInstanceState).apply {
+        dlg = (super.onCreateDialog(savedInstanceState).apply {
             // window?.setDimAmount(0.2f) // Set dim amount here
             setOnShowListener {
-                val bottomSheet = findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+                val bottomSheet =
+                    findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
                 bottomSheet.setBackgroundResource(android.R.color.transparent)
 
                 val behavior = BottomSheetBehavior.from(bottomSheet)
                 behavior.isDraggable = false
                 behavior.state = BottomSheetBehavior.STATE_EXPANDED
             }
-        } ) as BottomSheetDialog
+        }) as BottomSheetDialog
         return dlg
     }
 
@@ -78,16 +80,22 @@ class BottomAlarmReport(
         super.onViewCreated(view, savedInstanceState)
 
         val saveBtn = requireView().findViewById<TextView>(R.id.save_btn)
+        saveBtn.setBackgroundResource(R.drawable.custom_backgroundwhite_radius10)
+        saveBtn.isEnabled = false
         saveBtn.setOnClickListener {
-            when(spemType) {
-                is PeriodType.All -> callback.invoke("SPAM")
-                is PeriodType.One -> callback.invoke("SPAM")
-                is PeriodType.Three -> callback.invoke("SPAM")
-                is PeriodType.Six -> callback.invoke("SPAM")
+            when (spemType) {
+                is PeriodType.All ->
+                    callback.invoke("SPAM")
+                is PeriodType.One ->
+                    callback.invoke("SPAM")
+                is PeriodType.Three ->
+                    callback.invoke("SPAM")
+                is PeriodType.Six ->
+                    callback.invoke("SPAM")
                 is PeriodType.Year -> callback.invoke("SPAM")
-                else -> callback.invoke("SPAM")
             }
             dismiss()
+
         }
 
         val closeBtn = requireView().findViewById<ImageView>(R.id.close_btn)
@@ -96,36 +104,48 @@ class BottomAlarmReport(
         }
 
         all.setOnClickListener {
+            saveBtn.setBackgroundResource(R.drawable.custom_yellow_radius10)
+            saveBtn.isEnabled = true
             periodCheck(type = PeriodType.All)
             spemType = PeriodType.All
             requireActivity().hideKeyboard()
             editTextReport.clearFocus()
         }
         one.setOnClickListener {
+            saveBtn.setBackgroundResource(R.drawable.custom_yellow_radius10)
+            saveBtn.isEnabled = true
             periodCheck(type = PeriodType.One)
             spemType = PeriodType.One
             requireActivity().hideKeyboard()
             editTextReport.clearFocus()
         }
         three.setOnClickListener {
+            saveBtn.setBackgroundResource(R.drawable.custom_yellow_radius10)
+            saveBtn.isEnabled = true
             periodCheck(type = PeriodType.Three)
             spemType = PeriodType.Three
             requireActivity().hideKeyboard()
             editTextReport.clearFocus()
         }
         six.setOnClickListener {
+            saveBtn.setBackgroundResource(R.drawable.custom_yellow_radius10)
+            saveBtn.isEnabled = true
             periodCheck(type = PeriodType.Six)
             spemType = PeriodType.Six
             requireActivity().hideKeyboard()
             editTextReport.clearFocus()
         }
         year.setOnClickListener {
+            saveBtn.setBackgroundResource(R.drawable.custom_yellow_radius10)
+            saveBtn.isEnabled = true
             periodCheck(type = PeriodType.Year)
             spemType = PeriodType.Year
-            editTextReport.messageTextOnFocusChangeListener(requireContext(),linearLayoutEditTextReport )
+            editTextReport.messageTextOnFocusChangeListener(
+                requireContext(),
+                linearLayoutEditTextReport
+            )
             requireActivity().showKeyboard(editTextReport)
             editTextReport.requestFocus()
-
         }
     }
 
@@ -136,7 +156,7 @@ class BottomAlarmReport(
         sixCheck.isChecked = false
         yearCheck.isChecked = false
 
-        when(type) {
+        when (type) {
             is PeriodType.All -> allCheck.isChecked = true
             is PeriodType.One -> oneCheck.isChecked = true
             is PeriodType.Three -> threeCheck.isChecked = true
@@ -148,10 +168,10 @@ class BottomAlarmReport(
 }
 
 sealed class PeriodType {
-    object Init: PeriodType()
-    object All: PeriodType()
-    object One: PeriodType()
-    object Three: PeriodType()
-    object Six: PeriodType()
-    object Year: PeriodType()
+    object Init : PeriodType()
+    object All : PeriodType()
+    object One : PeriodType()
+    object Three : PeriodType()
+    object Six : PeriodType()
+    object Year : PeriodType()
 }
